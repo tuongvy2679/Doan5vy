526a185675b3f63d7bf771afbc0db4942aced902
tuongvy2679 <70678490+tuongvy2679@users.noreply.github.com>
2020-09-05 13:43:59 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ChitietComponent } from './chitiet/chitiet.component';
+import { DanhmucComponent } from './danhmuc/danhmuc.component';
+import{Routes,RouterModule} from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'chitiet',
+    component: ChitietComponent,
+  },
+  {
+    path: 'danhmuc',
+    component: DanhmucComponent,
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    ChitietComponent,
+    DanhmucComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
